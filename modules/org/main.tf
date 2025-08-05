4d11c961b737405c4af5f1395618cdd3256329de
Vitor Pinho <eng.vitorpinho@gmail.com>
2025-08-05 12:42:58 +0100

    feat(org): reference existing AWS Organization instead of creating

--- modules/org/main.tf
+++ modules/org/main.tf
@@ -1,12 +1,14 @@
 
-resource "aws_organizations_organization" "root_org" {
-  feature_set = "ALL"
-}
+#resource "aws_organizations_organization" "root_org" {
+#  feature_set = "ALL"
+#}
+data "aws_organizations_organization" "current" {}
 
 resource "aws_organizations_organizational_unit" "ou" {
-  for_each  = toset(var.organizational_units)
-  name      = each.value
-  parent_id = aws_organizations_organization.root_org.roots[0].id
+  for_each = toset(var.organizational_units)
+  name     = each.value
+  #parent_id = aws_organizations_organization.root_org.roots[0].id
+  parent_id = data.aws_organizations_organization.current.roots[0].id
 }
 
 resource "aws_organizations_policy_attachment" "attach_scp" {
