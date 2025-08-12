5e497046258f6eb23049e9c5bfb0236b36287456
Vitor Pinho <eng.vitorpinho@gmail.com>
2025-08-12 14:47:54 +0100

    feat(network): add complete network module with variables, outputs, and documentation

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,3 +20,54 @@ module "org" {
     DenyGlobalServices = "p-57oic78t"
   }
 }
+
+module "network" {
+  source = "../../modules/network"
+
+  # General naming
+  prefix = "dev"
+
+  # Hub VPC
+  hub_vpc_cidr = "10.0.0.0/16"
+  hub_private_subnet_cidrs = {
+    "eu-west-1a" = "10.0.1.0/24"
+    "eu-west-1b" = "10.0.2.0/24"
+  }
+  hub_public_subnet_cidrs = {
+    "eu-west-1a" = "10.0.101.0/24"
+    "eu-west-1b" = "10.0.102.0/24"
+  }
+  hub_nat_strategy            = "single"
+  hub_create_internet_gateway = true
+
+  # Transit Gateway
+  create_transit_gateway               = true
+  tgw_auto_accept_shared_attachments   = "enable"
+  tgw_default_route_table_association  = "enable"
+  tgw_default_route_table_propagation  = "enable"
+
+  # Example spoke
+  spokes = [
+    {
+      name                    = "spoke1"
+      vpc_cidr                = "10.1.0.0/16"
+      private_subnet_cidrs    = { "eu-west-1a" = "10.1.1.0/24" }
+      public_subnet_cidrs     = { "eu-west-1a" = "10.1.101.0/24" }
+      create_internet_gateway = true
+      nat_strategy            = "single"
+    }
+  ]
+
+  # Tags applied to all resources
+  tags = {
+    Project     = "cloudguard-landingzone"
+    Environment = "dev"
+  }
+
+  # Enable VPC Flow Logs (to CloudWatch)
+  enable_vpc_flow_logs       = true
+  flow_logs_destination_type = "cloudwatch"
+  flow_logs_log_group_name   = "/aws/vpc/flowlogs/hub"
+  flow_logs_retention_days   = 90
+  # flow_logs_kms_key_id     = "arn:aws:kms:eu-west-1:123456789012:key/your-key-id" # optional
+}
